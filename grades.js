7467f744e6ac71cbdb3cf14d4c6b8540ce379c98
yukidaruma2872 <y.yuki8002@gmail.com>
2026-08-25 00:44:53 +0900

    syle:グラフの削除、ボタンの位置の変更

--- grades.js
+++ grades.js
@@ -1,13 +1,3 @@
-/*成績データをCSS→HTMLに反映 */
-const percentageCorrect = getComputedStyle(document.documentElement).getPropertyValue("--correct");
-document.getElementById("percentage-correct").textContent = percentageCorrect.trim();
-
-const percentageIncorrect = getComputedStyle(document.documentElement).getPropertyValue("--incorrect");
-document.getElementById("percentage-incorrect").textContent = percentageIncorrect.trim();
-
-const percentageUnanswered = getComputedStyle(document.documentElement).getPropertyValue("--unanswered");
-document.getElementById("percentage-unanswered").textContent = percentageUnanswered.trim();
-
 const wordbtn = document.getElementById("wordbtn");
 
 const wordlist = document.getElementById("itiran");
